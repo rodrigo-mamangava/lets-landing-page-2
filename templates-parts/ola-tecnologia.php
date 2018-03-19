00a7e54de2b39ba5e1b7e2cf7c1b5f83cd8e412a
Samuel Brandão <gb.samuel@gmail.com>
2018-03-19 17:52:30 -0300

    Adjust vertical centering of title and paragraph on the first page

--- templates-parts/ola-tecnologia.php
+++ templates-parts/ola-tecnologia.php
@@ -1,20 +1,20 @@
-
 <section id="ola-tecnologia" class="lazy" data-src="<?php echo get_field('background_ola_tecnologia')?>"   >
 
     <?php include 'navbar.php'; ?>
-
     <div class="container">
         <div class="row">
             <div class="col-md-6">
-                <h1>
-                    <?php echo get_field('saudacao')?>
-                    <span id="areas-atuacao"></span>
-                </h1>
-                <p>
-                    <?php echo get_field('paragrafo_ola_tecnologia')?>
-                </p>
+                <div class='page-content'>
+                    <h1>
+                        <?php echo get_field('saudacao')?>
+                        <span id="areas-atuacao"></span>
+                    </h1>
+                    <p>
+                        <?php echo get_field('paragrafo_ola_tecnologia')?>
+                    </p>
 
-                <button class="btn-verde uppercase"> <?php echo get_field('botao-ola-tecnologia')?></button>
+                    <button class="btn-verde uppercase"> <?php echo get_field('botao-ola-tecnologia')?></button>
+                </div>
             </div>
         </div>
     </div>
@@ -26,16 +26,11 @@
             <i class="fas fa-chevron-down"></i>
         </p>
     </a>
-
-
-
 </section><!-- ola-tecnologia -->
-
 
 <script>
 $(function () {
     var areas = $("#areas-atuacao").html();
-    console.log(areas);
     var palavras = "<?php echo get_field('areas-atuacao')?>"  ;   
     var listaPalavras = palavras.split(',');
     changeWords(listaPalavras, 2000);
