3bfc704b909781f13cde738f87031655e797ee20
Samuel Brandão <gb.samuel@gmail.com>
2018-03-19 18:21:01 -0300

    Remove the pinBackground js and fix the effect issues

--- js/main.js
+++ js/main.js
@@ -1,38 +1,7 @@
 $(function () {
     $('.lazy').Lazy();
     scrollSmooth();
-    pinBackground();
 });
-
-//http://jquery.eisbehr.de/lazy/
-
-function pinBackground() {
-    $(window).scroll(function () {
-
-        var pxScrollado = $(window).scrollTop();
-        var inicioFix = 1000;
-        var fimFix = 2100;
-
-        console.log("pxScrollado", pxScrollado);
-
-        //After scrolling 100px from the top...
-        if (pxScrollado <= inicioFix) {
-            $('#venda-mais').css('background-attachment', 'scroll');
-            $('#sua-lista').css('background-attachment', 'scroll');
-            $('#formidaveis').css('background-attachment', 'scroll');
-
-        } else if ((pxScrollado >= inicioFix) && (pxScrollado <= fimFix)) {
-            $('#venda-mais').css('background-attachment', 'fixed');
-            $('#sua-lista').css('background-attachment', 'fixed');
-            $('#formidaveis').css('background-attachment', 'fixed');
-
-        } else {
-            $('#formidaveis').css('background-attachment', 'scroll');
-        }
-
-    });
-}
-
 
 function changeWords(wordsArray, intervalo) {
     var $areas = $("#areas-atuacao").text(wordsArray[0]);
