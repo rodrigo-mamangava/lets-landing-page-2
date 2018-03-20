e7fce746af699ac9786ae4f4596982fb2d540dc6
Samuel Brandão <gb.samuel@gmail.com>
2018-03-20 15:04:47 -0300

    Adjust size and position for images on mobile display size

--- templates-parts/formidaveis.php
+++ templates-parts/formidaveis.php
@@ -3,10 +3,10 @@
         <div class="row">
 
             <div class="col-xs-12 visible-xs visible-sm">
-                <img data-src="<?php echo plugins_url('../img/ingresso-lista@2x.png', __FILE__); ?>" alt="" class="img-responsive">
+                <img src="<?php echo plugins_url('../img/ingresso-lista@2x.png', __FILE__); ?>" alt="" class="img-responsive">
             </div>
 
-            <div class="col-md-7 col-md-offset-5 col-lg-6 col-lg-offset-6">
+            <div class="col-xs-12 col-md-7 col-md-offset-5 col-lg-6 col-lg-offset-6">
                 <h2>
                     <?php echo get_field('formidaveis_em_conjunto_titulo') ?>
                 </h2>
