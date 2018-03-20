1700d9171c3dfde337ae50e2ec1cb30fff49fd0c
Samuel Brandão <gb.samuel@gmail.com>
2018-03-19 21:50:47 -0300

    Add google maps "Belzonte" link

--- templates-parts/form.php
+++ templates-parts/form.php
@@ -19,9 +19,7 @@
             <div class="col-xs-12 col-sm-6">
                 <p >
                     De
-                    <a href="">
-                        Belzonte
-                    </a>
+                    <a href="https://www.google.it/maps/place/Belo+Horizonte,+MG/@-20.2506648,-65.1489347,3.5z/data=!4m5!3m4!1s0xa690cacacf2c33:0x5b35795e3ad23997!8m2!3d-19.9217127!4d-43.9343262" target="_blank">Belzonte</a>
                     <?php _traduzir('to the world', 'para o Mundo')?>
                     
                     <img src="<?php echo plugins_url('../img/Brasil-BH@2x.png', __FILE__) ?>" alt=""> Made in Brasil
